99e460b5ed7eb20b2a4d66b31ed04c0b99066d3c
nirmalamala25 <mala27793@gmail.com>
2023-12-05 16:26:57 +0700

    tugas-akhir

---
+++ ecommerce/composables/constants/category.ts
@@ -0,0 +1,54 @@
+export const category: Category[] = [
+    {
+    id: 1,
+    name: "Bunga Mawar",
+    },
+    {
+    id: 2,
+    name: "Bunga Matahari",
+    },
+    {
+    id: 3,
+    name: "Bunga Melati",
+    },
+    {
+    id: 4,
+    name: "Bunga Lilly",
+    },
+    {
+    id: 5,
+    name: "Bunga Tulip",
+    },
+    {
+    id: 6,
+    name: "Bunga Bougenville",
+    },
+    {
+    id: 7,
+    name: "Bunga Dahlia",
+    },
+    {
+    id: 8,
+    name: "Bunga Lavender",
+    },
+    {
+    id: 9,
+    name: "Bunga Anyelir",
+    },
+    {
+    id: 10,
+    name: "Bunga Teratai",
+    },
+    {
+    id: 11,
+    name: "Bunga Daisy",
+    },
+    {
+    id: 12,
+    name: "Bunga Kamboja",
+    },
+    ]
+    export interface Category {
+    id? : number,
+    name?: string,
+    }
